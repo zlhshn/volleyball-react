df005f81b6f04122a56c539c179b521a94ec3030
zlhshn <zsahin4401@gmail.com>
2023-12-03 17:42:42 +0300

    data added

---
+++ src/data.js
@@ -0,0 +1,143 @@
+export const data = [
+    {
+      name: "Hande Baladın",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY_K9Gw70k2XmKdTs9CkOIwV4cSdKI-NTlVw&usqp=CAU",
+      statistics: [
+        "Smaçör",
+        "01/09/1997",
+        "81 kg",
+        "190 cm",
+      ],
+    },
+    {
+      name: "İlkin Aydın",
+      img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fpolemikhabercom.teimg.com%2Fpolemikhaber-com%2Fuploads%2F2023%2F07%2Filkin-aydin.jpg&tbnid=eRD-pGdvl6Z2fM&vet=10CFEQMyiJAWoXChMImLTdhLjzggMVAAAAAB0AAAAAEAM..i&imgrefurl=https%3A%2F%2Fwww.polemikhaber.com%2Filkin-aydindan-trt-muhabirine-nasil-bir-soru-bu&docid=8N49A7UZmafgYM&w=860&h=503&q=ilkin%20ayd%C4%B1n&ved=0CFEQMyiJAWoXChMImLTdhLjzggMVAAAAAB0AAAAAEAM",
+      statistics: [
+        "Smaçör",
+        "05/01/2000",
+        "67 kg",
+        "183 cm",
+      ],
+    },
+    {
+      name: "Kübra Akman",
+      img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftvf.org.tr%2Fwp-content%2Fuploads%2F2023%2F05%2FKubra-Akman.jpg&tbnid=fk7Jjg9BEBOpkM&vet=12ahUKEwjRg92nuPOCAxX0ReUKHcNuC4YQMygCegQIARBB..i&imgrefurl=https%3A%2F%2Ftvf.org.tr%2Fa-milli-kadin-takimi-kadro%2Fattachment%2Fkubra-akman-3%2F&docid=yWYFQYdOy0iBIM&w=1127&h=1600&q=k%C3%BCbra%20akman&ved=2ahUKEwjRg92nuPOCAxX0ReUKHcNuC4YQMygCegQIARBB",
+      statistics: [
+        "Orta Oyuncu",
+        "13/10/1994",
+        "88 kg",
+        "200 cm",
+      ],
+    },
+    {
+      name: "Melissa Vargas",
+      img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftvf.org.tr%2Fwp-content%2Fuploads%2F2023%2F05%2FMelissa-Vargas.jpg&tbnid=36KwiudYk3MaAM&vet=12ahUKEwi7mfKzuPOCAxXZybsIHTFSDlsQMygDegQIARBb..i&imgrefurl=https%3A%2F%2Ftvf.org.tr%2Fa-milli-kadin-takimi-kadro%2Fattachment%2Fmelissa-vargas-3%2F&docid=bwZyt48D6q15HM&w=994&h=1280&q=melissa%20vargas&ved=2ahUKEwi7mfKzuPOCAxXZybsIHTFSDlsQMygDegQIARBb",
+      statistics: [
+        "Pasör Çaprazı",
+        "16/10/1999",
+        "76 kg",
+        "194 cm",
+      ],
+    },
+    {
+      name: "Zehra Güneş",
+      img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftvf.org.tr%2Fwp-content%2Fuploads%2F2021%2F05%2FZEHRA-GUNES-scaled.jpg&tbnid=KVQWa0kROXNOXM&vet=12ahUKEwjNpN68uPOCAxXBqf0HHfHzD7YQMygGegQIARBy..i&imgrefurl=https%3A%2F%2Ftvf.org.tr%2Fa-milli-kadin-takimi-kadro%2Fattachment%2Fzehra-gunes-5%2F&docid=MWIX9OynbkuleM&w=1646&h=2560&q=zehra%20g%C3%BCne%C5%9F&ved=2ahUKEwjNpN68uPOCAxXBqf0HHfHzD7YQMygGegQIARBy",
+      statistics: [
+        "Orta Oyuncu",
+        "07/07/1999",
+        "88 kg",
+        "197 cm",
+      ],
+    },
+    {
+      name: "Simge Aköz",
+      img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftvf.org.tr%2Fwp-content%2Fuploads%2F2021%2F05%2FSIMGE-AKOZ-scaled.jpg&tbnid=Lor89kmor0k1nM&vet=12ahUKEwjl68rEuPOCAxVMof0HHWmvC8cQMygBegQIARA9..i&imgrefurl=https%3A%2F%2Ftvf.org.tr%2Fa-milli-kadin-takimi-kadro%2Fattachment%2Fsimge-akoz-5%2F&docid=RsQLmxNZNkNpRM&w=1646&h=2560&q=simge%20ak%C3%B6z&ved=2ahUKEwjl68rEuPOCAxVMof0HHWmvC8cQMygBegQIARA9",
+      statistics: [
+        "Libero",
+        "23/04/1991",
+        "55 kg",
+        "168 cm",
+      ],
+    },
+    {
+      name: "Cansu Özbay",
+      img: "https://tvf.org.tr/wp-content/uploads/2023/08/Cansu_Ozbay_0005.jpg",
+      statistics: [
+        "Pasör",
+        "17/10/1996",
+        "78 kg",
+        "182 cm",
+      ],
+    },
+    {
+      name: "Eda Erdem Dündar",
+      img: "https://tvf.org.tr/wp-content/uploads/2021/05/EDA-ERDEM-DUNDAR-1-scaled.jpg",
+      statistics: [
+        "Orta Oyuncu",
+        "22/06/1987",
+        "74 kg",
+        "188 cm",
+      ],
+    },
+    {
+      name: "Ebrar Karakurt",
+      img: "https://tvf.org.tr/wp-content/uploads/2021/05/EBRAR-KARAKURT-scaled.jpg",
+      statistics: [
+        "Smaçör",
+        "17/01/2000",
+        "73 kg",
+        "196 cm",
+      ],
+    },
+    {
+      name: "Ayça Aykaç",
+      img: "https://cevadmin.cev.eu/FTPFiles/Media/Objects/48437/Form04_80947_Original.jpg",
+      statistics: [
+        "Libero",
+        "27/02/1996",
+        "57 kg",
+        "176 cm",
+      ],
+    },
+    {
+      name: "Gizem Örget",
+      img: "https://tvf.org.tr/wp-content/uploads/2023/05/Gizem-Orge.jpg",
+      statistics: [
+        "Libero",
+        "26/04/1993",
+        "59 kg",
+        "170 cm",
+      ],
+    },
+    {
+      name: "Aslı Kalaç",
+      img: "https://voleybolunsesi.com/uploadk33/2023/03/IMG_3005-e1678377247895.jpg",
+      statistics: [
+        "Orta Oyuncu",
+        "13/12/1995",
+        "73 kg",
+        "185 cm",
+      ],
+    },
+    {
+      name: "Elif Şahin",
+      img: "https://tvf.org.tr/wp-content/uploads/2022/08/Elif-Sahin-scaled.jpg",
+      statistics: [
+        " Pasör",
+        "19/01/2001",
+        "68 kg",
+        "190 cm",
+      ],
+    },
+    {
+      name: "Derya Cebecioğlu",
+      img: "https://tvf.org.tr/wp-content/uploads/2022/08/derya-cebecioglu-1-1.png",
+      statistics: [
+        "Smaçör",
+        "24/10/2000",
+        "68 kg",
+        "187 cm",
+      ],
+    },
+  ];
+  
