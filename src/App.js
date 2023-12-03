da4b98c552608999b7f59b9ed09803820031e663
zlhshn <zsahin4401@gmail.com>
2023-12-03 17:52:01 +0300

    component add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+import  "./app.css"
 
 function App() {
   return (
     < >
 
     <Header/>
-    
+    <Navbar/>
+    <Main/>
+
     
     </>
   );
