7be21e01010584be1f4adf826736ec5dc33374fb
zlhshn <zsahin4401@gmail.com>
2023-12-09 20:14:52 +0300

    app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from   "../src/components/Header";
 import Navbar from  "../src/components/Navbar";
-import Container from "react-bootstrap/Container";
 import "./app.css";
 
 function App() {
